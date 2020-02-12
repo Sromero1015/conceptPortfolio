a626e657483c79e150268de3daacbd7d2975adb6
Sergio Romero <sromerojr1015@gmail.com>
2020-02-12 16:59:23 -0500

    site functions implemented

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+//click event for about me button
+$('#aboutMe').on('click',function(){
+    console.log('About Me: clicked');
+});
+
+//click event for projects button
+$('#projects').on('click', function(){
+    console.log('Project: clicked');
+});
+
+//click event for contact button
+$('#contact').on('click', function(){
+    console.log('Contact: clicked');
+});
